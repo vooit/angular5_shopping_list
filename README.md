e393d0bcd16bb7941589774751b33973875a6033
vooit <wojciechpac@gmail.com>
2018-04-26 16:13:20 +0200

    element on click shows data

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# N5CompleteGuide
-
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.5.0.
 
 ## Development server
--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import {Component, OnInit, Input, EventEmitter, Output} from '@angular/core';
 
-import { Recipe } from '../../recipe.model';
+import {Recipe} from '../../recipe.model';
 
 @Component({
-  selector: 'app-recipe-item',
-  templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+    selector: 'app-recipe-item',
+    templateUrl: './recipe-item.component.html',
+    styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-  @Input() recipe: Recipe;
-  @Output() recipeSelected = new EventEmitter<void>();
+    @Input() recipe: Recipe;
+    @Output() recipeSelected = new EventEmitter<void>();
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  onSelected() {
-    this.recipeSelected.emit();
-  }
+
+    onSelected() {
+        this.recipeSelected.emit();
+    }
+
+
+    // onSelected() {
+    //   this.recipeSelected.emit();
+    // }
 
 }
